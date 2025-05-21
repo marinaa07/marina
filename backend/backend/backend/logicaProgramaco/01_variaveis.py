bb5aa8750b61dd2f18a09ff9f684fd1c143242c5
marina valezi <marina.valeze@escola.pr.gov.br>
2025-05-21 11:46:42 -0300

    iniciando logica de programacao

---
+++ backend/backend/backend/logicaProgramaco/01_variaveis.py
@@ -0,0 +1,30 @@
+nome= "marina"
+idade= 18
+salario= 850.000
+legal= True
+
+print("nome", nome)
+print("idade", idade)
+print("salario", salario)
+print("élegal?", legal)
+
+animal= "gato"
+local= "restaurante"
+peso= "77 kg"
+altura= 1.77
+apartamento= 5 
+cauculadora= 156.97
+suamaeebonita= True
+suacomidaeboa= False
+valor= 10
+
+
+print("Animal", animal)
+print("Local", local)
+print("Peso", peso)
+print("Altura", altura)
+print("Apartamento", apartamento)
+print("Cauculadora", cauculadora)
+print("SuaMaeEBonita", suamaeebonita)
+print("Valor", valor)
+print("SuaComidaEBoa", suacomidaeboa)
---
+++ backend/backend/backend/logicaProgramaco/02_operacoesMatematicas.py
@@ -0,0 +1,30 @@
+A = 10 
+B = 44
+soma= A + B 
+
+print ("Soma de A + B é:", soma)
+
+subtracao= A - B 
+print("subtracao A - B é:", subtracao)
+
+multiplicacao= A * B 
+print("multiplicacao A * B é:",multiplicacao)
+
+divisao= A / B 
+print("divisao  A / B é:", divisao) 
+
+marina= 88
+valezi= 876
+
+soma = marina + valezi
+print("soma de marina + valezi é", soma)
+
+
+subtracao= marina - valezi
+print("subtracao marina - valezi é", subtracao)
+
+multiplicacao= marina * valezi
+print("multiplicacao marina * valezi é:", multiplicacao)
+
+divisao= marina / valezi
+print("divisao marina / valezi é:", divisao)
